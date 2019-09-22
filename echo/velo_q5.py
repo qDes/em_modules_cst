315d4ce3d967e30f6808895cd30b303950ea9bc5
Andrei K <1____1@bk.ru>
2019-09-22 19:11:52 +0300

    add all widgets

--- echo/velo_q5.py
+++ echo/velo_q5.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import *
 
 import time
 import socket
+import struct
 
 class MainWindow(QMainWindow):
 
@@ -13,20 +14,52 @@ class MainWindow(QMainWindow):
         
         self.sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        self.counter = 0
-    
+        #self.counter = 0
+        self.pack = struct.pack(">3c8f",b"C", b"2", b"H", 20, 0.006, 10,
+                20, 1.1, 1, 5 ,60)
         layout = QVBoxLayout()
         
-        self.l = QLabel("Start")
+        #self.l = QLabel("Start")
         b = QPushButton("DANGER!")
         b.pressed.connect(self.oh_no)
         
-        self.textbox = QLineEdit(self)
+        self.f_set_label = QLabel("F-set")
+        self.f_set_box = QLineEdit('20')
+        self.kShaker_label = QLabel("kShaker")
+        self.kShaker_box = QLineEdit('0.006')
+        self.shaker_freq_label = QLabel("shaker_freq")
+        self.shaker_freq_box = QLineEdit('10')
+        self.m_label = QLabel('m-inner')
+        self.m_box = QLineEdit('20')
+        self.kPedal_label = QLabel('kPedal')
+        self.kPedal_box = QLineEdit('1.1')
+        self.shaker_limit_label = QLabel('shaker limit')
+        self.shaker_limit_box = QLineEdit('1')
+        self.friction_label = QLabel('friction')
+        self.friction_box = QLineEdit('5')
+        self.p_set_label = QLabel('p-set')
+        self.p_set_box = QLineEdit('60')
+        
 
-
-        layout.addWidget(self.l)
+        #layout.addWidget(self.l)
         layout.addWidget(b)
-        layout.addWidget(self.textbox)
+        
+        layout.addWidget(self.f_set_label)
+        layout.addWidget(self.f_set_box)
+        layout.addWidget(self.kShaker_label)
+        layout.addWidget(self.kShaker_box)
+        layout.addWidget(self.shaker_freq_label)
+        layout.addWidget(self.shaker_freq_box)
+        layout.addWidget(self.m_label)
+        layout.addWidget(self.m_box)
+        layout.addWidget(self.kPedal_label)
+        layout.addWidget(self.kPedal_box)
+        layout.addWidget(self.shaker_limit_label)
+        layout.addWidget(self.shaker_limit_box)
+        layout.addWidget(self.friction_label)
+        layout.addWidget(self.friction_box)
+        layout.addWidget(self.p_set_label)
+        layout.addWidget(self.p_set_box)
 
         w = QWidget()
         w.setLayout(layout)
@@ -36,20 +69,34 @@ class MainWindow(QMainWindow):
         self.show()
 
         self.timer = QTimer()
-        self.timer.setInterval(1000)
+        self.timer.setInterval(50)
         self.timer.timeout.connect(self.recurring_timer)
         self.timer.start()
     
 
     def oh_no(self):
         #time.sleep(5)
-        textboxValue = int(self.textbox.text())
-        self.counter += textboxValue#20
+        f_set = float(self.f_set_box.text())
+        kShaker = float(self.kShaker_box.text())
+        shaker_freq = float(self.shaker_freq_box.text())
+        m = float(self.m_box.text())
+        kPedal = float(self.kPedal_box.text())
+        shaker_limit = float(self.shaker_limit_box.text())
+        friction = float(self.friction_box.text())
+        p_set = float(self.p_set_box.text())
+        self.pack = struct.pack(">3c8f",b"C", b"2", b"H", f_set, kShaker, 
+                shaker_freq, m, kPedal, shaker_limit, friction,
+                p_set)
+ 
+        #self.counter += textboxValue#20
+
+    
+
 
     def recurring_timer(self):
-        self.counter +=1
-        self.sock_send.sendto(str(self.counter).encode(encoding='utf-8'),('127.0.0.1',5500))
-        self.l.setText("Counter: %d" % self.counter)
+        #self.counter +=1
+        self.sock_send.sendto(self.pack,('127.0.0.1',5500))
+        #self.l.setText("Counter: %d" % self.counter)
     
     
 app = QApplication([])
