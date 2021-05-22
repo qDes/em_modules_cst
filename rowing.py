9cd89cd0f7147e01d591c41f4a9f4d7ffd5b92b3
Andrei <andrei.kozlov.m@gmail.com>
2021-05-22 13:55:00 +0300

    add common features; 3 plots

--- rowing.py
+++ rowing.py
@@ -1,7 +1,9 @@
+import datetime
 import json
 
 from dearpygui.core import *
 from dearpygui.simple import *
+import pyscreenshot as ImageGrab
 
 from util_ import UDP, Plotter, PlotSaver
 
@@ -57,13 +59,19 @@ def plot_callback():
             return
         y1, y2, y3, y4 = y[0], y[1], y[5], y[6]
         plot.update(get_delta_time(), y1, y2, y3, y4)
-        clear_plot("Plot")
+        # clear_plot("Plot")
         add_line_series("Plot", "F0", plot.x1, plot.y1, weight=2, axis=0)
         add_line_series("Plot", "pos0", plot.x2, plot.y2, weight=2, axis=1)
-        add_line_series("Plot", "F1", plot.x3, plot.y3, weight=2, axis=0)
-        add_line_series("Plot", "pos1", plot.x4, plot.y4, weight=2, axis=1)
+        add_line_series("Plot1", "F1", plot.x3, plot.y3, weight=2, axis=0)
+        add_line_series("Plot1", "pos1", plot.x4, plot.y4, weight=2, axis=1)
+
+        add_line_series("Plot2", name='Power1, W', x=plot.px, y=plot.p1, weight=2, axis=0)
+        add_line_series("Plot2", name='Power2, W', x=plot.px, y=plot.p2, weight=2, axis=0)
+        print(len(plot.p1), len(plot.p2), len(plot.px))
+
         if recorder.is_saving:
             recorder.get_data(y1, y2, y3, y4)
+
 
 def save_params(sender, data):
     i0, a, b, c, d, e, f, m_inner, kOut_mode0, kOut_mode1 = get_data("", "")
@@ -92,24 +100,42 @@ def close_help():
 
 
 def start_record():
+    configure_item("Start record", enabled=False)
     recorder.start()
 
 
 def stop_record():
+    configure_item("Start record", enabled=True)
     recorder.stop()
+
+
+def make_screenshot():
+    # grab fullscreen
+    im = ImageGrab.grab()
+
+    # save image file
+    im.save(
+        f"/Users/a18351639/projects/em_modules_cst/screenshots/rowing_{datetime.datetime.now().strftime('%y-%m-%d_%H-%M-%S')}.png")
+
+
+def set_plot_time():
+    clear_plot("Plot")
+    plot_len = int(get_value("plot_time")) * 50
+    plot.update_limit(plot_len)
+    plot.lim = plot_len
 
 
 with window("Main Window"):
     with group("Left Panel", width=250):
         # add_button("Plot data", callback=plot_callback)
-        add_text("Connection params")
+        add_text("Connection parameters")
 
-        add_input_text("Address", source="address", default_value="192.168.0.193", width=200)
-        #add_input_text("Address", source="address", default_value="192.168.31.149", width=200)
+        # add_input_text("Address", source="address", default_value="192.168.0.193", width=200)
+        add_input_text("Address", source="address", default_value="192.168.31.149", width=200)
         add_button("Connect", callback=connect)
         add_button("Disconnect", callback=disconnect)
         ## Params
-        add_text("Model params")
+        add_text("Model parameters")
         add_listbox("mode", source="i0", default_value=0, items=["0", "1", "2"])
         add_input_text("a", source="a", default_value="0.1", width=200)
         add_input_text("b", source="b", default_value="10.0", width=200)
@@ -121,16 +147,21 @@ with window("Main Window"):
         add_input_text("kOut_mode0", source="kOut_mode0", default_value="1.0", width=200)
         add_input_text("kOut_mode1", source="kOut_mode1", default_value="0.0", width=200)
 
-        add_button("Set params", callback=setup_params)
-        add_spacing(count=10)
-        add_button("Save params", callback=save_params)
-        add_button("Load params", callback=load_params)
-        add_spacing(count=10)
+        add_button("Set parameters", callback=setup_params)
+        add_spacing(count=3)
+        add_button("Save parameters", callback=save_params)
+        add_button("Load parameters", callback=load_params)
+        add_spacing(count=3)
         add_button("Start record", callback=start_record)
         add_button("Stop record", callback=stop_record)
-        add_spacing(count=10)
+        add_spacing(count=3)
         add_button("Help")
+        add_spacing(count=3)
+        add_button("Screenshot", callback=make_screenshot)
 
+        add_spacing(count=3)
+        add_input_text("s", source="plot_time", default_value="10", width=50)
+        add_button("Set plot time", callback=set_plot_time)
         with popup("Help", 'Help Popup', modal=True, mousebutton=mvMouseButton_Left):
             with open(HELP, 'r') as my_file:
                 help_data = my_file.read()
@@ -138,7 +169,14 @@ with window("Main Window"):
             add_button("Close", callback=close_help)
 
     add_same_line()
-    add_plot("Plot", height=-1, x_axis_name="Training time, s", yaxis2=True)
+
+    with tab_bar("Plots"):
+        with tab("Plot 1"):
+            add_plot("Plot", height=-1, yaxis2=True, x_axis_name="Training time, s")
+        with tab("Plot 2"):
+            add_plot("Plot1", height=-1, yaxis2=True, x_axis_name="Training time, s")
+        with tab("Plot 3"):
+            add_plot("Plot2", height=-1, yaxis2=True, x_axis_name="Training time, s")
 
 if __name__ == "__main__":
     i0, a, b, c, d, e, f, m_inner, kOut_mode0, kOut_mode1 = get_data("", "")
