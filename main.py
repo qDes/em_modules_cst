a3546b900eaa62d2a2c971c6d561667d5e4efa3c
Andrei <andrei.kozlov.m@gmail.com>
2021-05-23 12:00:51 +0300

    update dir

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import pyscreenshot as ImageGrab
 
 from util_ import UDP, Plotter, PlotSaver
 
-PARAMS = "/Users/a18351639/projects/em_modules_cst/params/main.json"
-HELP = "/Users/a18351639/projects/em_modules_cst/params/main.help"
-RECORD_DIR = "/Users/a18351639/projects/em_modules_cst/plots"
+ROOT_DIR = "/Users/a18351639/projects"
+PARAMS = f"{ROOT_DIR}/em_modules_cst/params/main.json"
+HELP = f"{ROOT_DIR}/em_modules_cst/params/main.help"
+RECORD_DIR = f"{ROOT_DIR}/em_modules_cst/plots"
 
 COUNTER = 0
 
@@ -145,7 +146,7 @@ def make_screenshot():
 
     # save image file
     im.save(
-        f"/Users/a18351639/projects/em_modules_cst/screenshots/main_{datetime.datetime.now().strftime('%y-%m-%d_%H-%M-%S')}.png")
+        f"{ROOT_DIR}/em_modules_cst/screenshots/main_{datetime.datetime.now().strftime('%y-%m-%d_%H-%M-%S')}.png")
 
 
 def set_plot_time():
